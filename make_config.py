13a5f0483d75422fc63732ef266c0a8561590607
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2017-04-03 15:36:39 +0900

    名前の指定はpost_textをいじらなきゃいけなくなっていたので,configに名前をもたせることも記述.configだけいじればいいようになる

--- make_config.py
+++ make_config.py
@@ -1,5 +1,5 @@
 def make_config_file():
     f = open('config.py','w')
-    f.write('config = {\'slack_url\': \'XXXX\', \'api_key\': \'XXXX\'}')
+    f.write('config = {\'slack_url\': \'XXXX\', \'api_key\': \'XXXX\', \'name\':\'名無しさん\'}')
     f.close()
     print('prease enter slack_url from webhook and api key from wakatime in config file')
--- post_text.py
+++ post_text.py
@@ -2,8 +2,8 @@ import slackweb
 import basicAPIKey
 from config import config
 
-def make_text(datas,my_name = '名無しさん'):
-    string = "Yutoは"
+def make_text(datas):
+    string = config['name'] + "は"
     for i in range(len(datas)):
         string += str(datas[i]['name']) + "を" + str(datas[i]['percent']) + "%"
         if i != len(datas) - 1:
@@ -16,7 +16,7 @@ def make_text(datas,my_name = '名無しさん'):
 def post_text():
     slack = slackweb.Slack(url = config['slack_url'])
     data = basicAPIKey.languages_request()
-    text = make_text(data, my_name ='Yuto')
+    text = make_text(data)
     print(text)
     # slack.notify(text = text)
 

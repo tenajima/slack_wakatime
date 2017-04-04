be5df745129b7fab3db2a86d66c8a2deeb66cd59
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2017-04-04 14:21:28 +0900

    叩くapiをstatsに変更  総コーディング時間も書き出せるように.

--- post_text.py
+++ post_text.py
@@ -2,11 +2,13 @@ import slackweb
 import basicAPIKey
 from config import config
 
-def make_text(datas):
-    string = config['name'] + "は"
-    for i in range(len(datas)):
-        string += str(datas[i]['name']) + "を" + str(datas[i]['percent']) + "%"
-        if i != len(datas) - 1:
+def make_text(data):
+    string = config['name'] + "の過去7日間の総コーディング時間は"
+    string += data['data']['human_readable_total'] + 'で,内訳は,'
+    languages = data['data']['languages']
+    for i in range(len(languages)):
+        string += str(languages[i]['name']) + "を" + str(languages[i]['percent']) + "%"
+        if i != len(languages) - 1:
             string += ","
     string += "書きました."
     # print(string)
@@ -18,7 +20,7 @@ def post_text():
     data = basicAPIKey.languages_request()
     text = make_text(data)
     print(text)
-    # slack.notify(text = text)
+    slack.notify(text = text)
 
 if __name__ == '__main__':
     post_text()
